755cbfb8fb13f4b48d6be18add4580f70d20f2e7
Mir Lutfur Rahman <mirlutfur.rahman@gmail.com>
2020-03-10 16:13:07 +0600

    s2

--- index.php
+++ index.php
@@ -2,3 +2,11 @@
 	<input type="text" name="word">
 	<input type="submit" name="submit" value="Search">
 </form>
+<h1 id="x"></h1>
+<script>
+	//document.getElementById('x').innerHTML="Hello";
+	var a=10,i;
+	for(i=1;i<=10;i++)
+		console.log(a+i);
+	
+</script>
